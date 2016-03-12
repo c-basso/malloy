9ff41b2bf90d600c120483c717f77c0f3fc9de58
Vlad C-basso <c-basso@ya.ru>
2016-03-13 01:34:35 +0300

    hodor

--- templates/auth.js
+++
@@ -1,8 +0,0 @@
-html
-meta(charset='utf8')
-form(action="https://money.yandex.ru/oauth/authorize" method="POST") 
-  input(type="hidden" name="client_id" value=client_id)
-  input(type="hidden" name="response_type" value='code')
-  input(type="hidden" name="redirect_uri" value=redirect_uri)
-  input(type="hidden" name="scope" value=scope)
-  button(type='submit') Авторизовать бота
